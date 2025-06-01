4f766f0db475234c1189faf1b1a488da3d1bb786
adityakashyap5047 <adityakashyap5047@gmail.com>
2025-06-02 00:11:35 +0530

    basic code for the feel tired problem

--- FeelTired.java
+++ FeelTired.java
@@ -2,10 +2,24 @@ import java.util.*;
 
 public class FeelTired{
 
-    public static int caculateExercises(int energy, int[] drainedEnergy){
+    public static int caculateExercises(int energy, Integer[] drainedEnergy){
 
-        
+        Arrays.sort(drainedEnergy, Collections.reverseOrder());
 
+        int exercises = 0;
+
+        for (int i = 0; i < drainedEnergy.length; i++) {
+            if(energy - 2 * drainedEnergy[i] >= 0) {
+                energy -= 2 * drainedEnergy[i];
+                exercises += 2;
+            } else if (energy - drainedEnergy[i] >= 0) {
+                energy -= drainedEnergy[i];
+                exercises++;
+            } 
+        }
+        if (energy <= 0) {
+            return exercises;
+        }
         return -1;
     }
 
@@ -19,7 +33,7 @@ public class FeelTired{
         System.out.print("Enter the total number of Exercises: ");
         int exercise = sc.nextInt();
 
-        int drainedEnergy[] = new int[exercise];
+        Integer drainedEnergy[] = new Integer[exercise];
 
         for(int i = 0; i < exercise; i++) {
             System.out.print("Enter the energy drained by exercise " + (i + 1) + ": ");
@@ -29,5 +43,7 @@ public class FeelTired{
         sc.close();
 
         int result = caculateExercises(energy, drainedEnergy);
+
+        System.out.println(result);
     }
 }
